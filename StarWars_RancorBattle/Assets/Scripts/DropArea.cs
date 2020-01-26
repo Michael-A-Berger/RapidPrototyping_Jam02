595f2f5d11dc40defd90a926f81815d436aff6e3
Enan Munzar (RIT Student) <esm6840@ad.rit.edu>
2020-01-26 17:05:53 -0500

    Camera script!

---
+++ StarWars_RancorBattle/Assets/Scripts/DropArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropArea : MonoBehaviour
+{
+    Draggable contains;
+
+    Rect area;
+    private void Start()
+    {
+        area = (transform as RectTransform).rect;
+        area.x += transform.position.x;
+        area.y += transform.position.y;
+    }
+
+    public bool DropCheck(Vector2 position)
+    {
+        return area.Contains(position);
+    }
+
+    public void Attach(Draggable item)
+    {
+
+    }
+}
