b2e51603daca888c4a587255c575bc5d7a44191a
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-01-26 12:18:44 -0500

    Created test scene w/ bouncing ball (+ bouncing script)

---
+++ StarWars_RancorBattle/Assets/Scripts/Bouncing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    // Attributes
+    public Rigidbody rb;
+    public SphereCollider sc;
+
+    // Properties
+    private Vector3 prev;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        sc = GetComponent<SphereCollider>();
+        prev = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prev != transform.position)
+        {
+            Debug.Log("(X,Y,Z):\t(" + transform.position + ")");
+            prev = transform.position;
+        }
+    }
+
+    // On collision with "Ground" tagged objects
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Added force");
+            transform.Translate(0f, 0.01f, 0f);
+            rb.AddForce(new Vector3(0f, 200f, 0f));
+        }
+    }
+}
