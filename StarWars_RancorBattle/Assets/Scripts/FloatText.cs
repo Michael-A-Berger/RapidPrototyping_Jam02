87e63174713f335ba9f627ead8c315b83fbb3cc8
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-01-26 16:00:08 -0500

    Finished Floating Text prefab + Added spawner in berger scene

---
+++ StarWars_RancorBattle/Assets/Scripts/FloatText.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FloatText : MonoBehaviour
+{
+    // Attributes
+    public Camera mainCamera;
+    public string displayText = "Sample Text";
+    public float fontSize = 12f;
+    public Color displayColor = Color.black;
+    public float floatTime = 2f;
+    public float floatSpeed = 1f;
+    public Vector3 floatDirection = new Vector3(0f, 1f, 0f);
+    [Range(0, 1)] public float disappearPercentage = 0.5f;
+
+    // Properties
+    private TextMeshPro textPro;
+    private float startTime;
+    private float startFadeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // mainCamera = Camera.main;
+        textPro = GetComponent<TextMeshPro>();
+        startTime = Time.time;
+        startFadeTime = floatTime * disappearPercentage;
+
+        // Hiding the text initially so that it doesn't "Snap" into place for the player
+        textPro.color = new Color(0f, 0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // Getting the current time to live
+        float currentTime = Time.time;
+        float timeLeft = (startTime + floatTime) - currentTime;
+
+        // Changing the text properties
+        textPro.text = displayText;
+        textPro.fontSize = fontSize;
+        textPro.color = displayColor;
+        
+        // IF the time left is less than the fade time, make the text opaque
+        if (timeLeft < startFadeTime)
+        {
+            textPro.color = new Color(displayColor.r, displayColor.g, displayColor.b, timeLeft / startFadeTime); ;
+        }
+
+        // IF the main camera is not set, then do that
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+
+        // Rotating the object towards the camera
+        transform.forward = mainCamera.transform.forward;
+
+        // Translating in the movement direction
+        transform.Translate(floatDirection * floatSpeed * Time.deltaTime, Space.World);
+
+        // IF the time limit has been exceeded, delete this object
+        if (currentTime > startTime + floatTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ StarWars_RancorBattle/Assets/Scripts/dummy_TextSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummy_TextSpawner : MonoBehaviour
+{
+    // Attributes
+    public GameObject floatTextPrefab;
+
+    // Properties
+    private GameObject spawned;
+    private FloatText spawnedScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float x = Random.Range(-5f, 5f);
+            float y = Random.Range(1f, 5f);
+            float z = Random.Range(-5f, 5f);
+            spawned = Instantiate(floatTextPrefab, new Vector3(x, y, z), Quaternion.identity);
+            spawnedScript = spawned.GetComponent<FloatText>();
+            spawnedScript.fontSize = Random.Range(8f, 24f);
+            spawnedScript.displayText = "Instance #" + Random.Range(1, 1000);
+            spawnedScript.displayColor = new Color(Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f));
+        }
+    }
+}
