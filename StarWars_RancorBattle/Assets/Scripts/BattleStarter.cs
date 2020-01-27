7f659e81ca292bd36ee39a6ca7b241fafec46004
Michael Berger (RIT Student) <mxb3145@ad.rit.edu>
2020-01-27 15:13:16 -0500

    Created new scene (berger_battle-start-test) + Edited BattleStart script to spawn player prefabs in the order (provided from the Player Select scene)

---
+++ StarWars_RancorBattle/Assets/Scripts/BattleStarter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStarter : MonoBehaviour
+{
+    // Attributes
+    public List<string> players;
+    [SerializeField]
+    PlayerSelectManager teamSelector;
+    [SerializeField]
+    GameObject mandoPref, wookPref, jedPref, boPref;
+    public Vector3[] spawnLocations;
+    public string playerPrefsKey = "";
+    public bool debug = false;
+
+    // Properties
+    private List<GameObject> playerObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // players = teamSelector.players;
+
+        // IF the player prefs key name is not set, squack at the dev
+        if (playerPrefsKey.Length < 1)
+        {
+            if (debug) Debug.LogError("'playerPrefsKey' is undefined!");
+        }
+        // ELSE... (the player preferences key name is set)
+        else
+        {
+            // Getting the list of players to spawn
+            string toSpawn = PlayerPrefs.GetString(playerPrefsKey);
+            players = new List<string>(toSpawn.Split(','));
+
+            // Spawning the players
+            for (int num = 0; num < players.Count; num++)
+            {
+                // Defining the current prefab
+                GameObject current = null;
+
+                // SWITCH for each player type
+                switch (players[num])
+                {
+                    case "Mando":
+                        current = mandoPref;
+                        break;
+                    case "Wook":
+                        current = wookPref;
+                        break;
+                    case "Jed":
+                        current = jedPref;
+                        break;
+                    case "Bo":
+                        current = boPref;
+                        break;
+                    default:
+                        if (debug) Debug.LogError("Prefab type of, '" + players[num] + "' is not defined!");
+                        break;
+                }
+
+                // Spawning the current game object in the right spawn location
+                if (current != null)
+                {
+                    playerObjects.Add(Instantiate(current, spawnLocations[num], Quaternion.identity));
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StarWars_RancorBattle/Assets/Scripts/ConfirmPlayersButton.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConfirmPlayersButton : MonoBehaviour
+{
+    // Attributes
+    public string nextSceneName = "";
+    public PlayerSelectManager selectManager;
+    public string playerPrefsKey = "";
+    public bool debug = false;
+
+    /// <summary>
+    /// SaveSelectionToPreferences() - Saves the currently-selected player
+    /// draggable object order to the player preferences so that they can
+    /// be used in the next scene.
+    /// </summary>
+    public void SaveSelectionToPreferences()
+    {
+        // IF there is no selection made in the player select manager, log an error
+        if (selectManager.players.Count < 1)
+        {
+            if (debug) Debug.LogError("Player selection not made!");
+        }
+        // ELSE... (selection has been made)
+        else
+        {
+            // Saving the selection order to a string
+            string toSave = "";
+            for (int num = 0; num < selectManager.players.Count; num++)
+            {
+                toSave += selectManager.players[num];
+                if (num < selectManager.players.Count - 1)
+                {
+                    toSave += ",";
+                }
+            }
+
+            // IF the player preferences key name is not defined, squack at the dev
+            if (playerPrefsKey.Length < 1)
+            {
+                if (debug) Debug.LogError("'playerPrefsKey' is undefined!");
+            }
+            // ELSE... (the player preferences key name is defined)
+            else
+            {
+                // Saving the selection order string to the player preferences
+                PlayerPrefs.SetString(playerPrefsKey, toSave);
+            }
+        }
+    }
+
+    /// <summary>
+    /// ChangeScene() - Loads the scene defined by the 'nextSceneName' string.
+    /// </summary>
+    public void ChangeScene()
+    {
+        // IF the next scene name is not defined, yell at the developer
+        if (nextSceneName.Length < 1)
+        {
+            Debug.LogError("'nextSceneName' is undefined!");
+        }
+        //ELSE... (the next scene name is defined)
+        else
+        {
+            // Loading the next scene
+            SceneManager.LoadSceneAsync(nextSceneName);
+        }
+    }
+
+    /// <summary>
+    /// SaveAndChange() - Saves the player order and changes the scene.
+    /// </summary>
+    public void SaveAndChange()
+    {
+        SaveSelectionToPreferences();
+        ChangeScene();
+    }
+}
